36f6a10608afac38eadff876381403f0fec2ba8b
Kamran <KamranPirishzade@gmail.com>
2026-07-28 10:35:48 +0400

    adding dynamic colors

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.functional'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.functional";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Apply env class to <body> so CSS can target it
+const env = import.meta.env.VITE_APP_ENV ?? "development";
+document.body.classList.add(`env-${env}`);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
